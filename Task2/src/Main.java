f208f7e007692e6233c5c7afb5c450e507b2c63b
RakhimT <127768870+RakhimT@users.noreply.github.com>
2023-07-12 00:21:44 +0300

    Main.java

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int amount = 1000;
+        int rubles = 20; // (за каждые 20 рублей начисляются милли)
+        int mile = 1;
+        System.out.println(amount / rubles + " миль можно получить за 1000 рублей потраченные на билет");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        double a = 1030.5, r = 20;
+        double m = a / r;
+        System.out.println(m); // 51.525 миль можно получить за 1030.5 рублей потраченные на билет
+
+
     }
 }
